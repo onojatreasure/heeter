702c906312d71e23df50a6c949351b24219b69af
Onoja Treasure <onojatreasure@gmail.com>
2020-04-11 10:19:04 +0100

    Installed laravel and designed timeline

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,21 @@
+<div class="flex p-4 border-b-gray-400">
+    <div class="mr-2 flex-shrink-8">
+        <img
+            src="https://i.pravatar.cc/50"
+            alt=""
+            class="rounded-full mr-2"
+        >
+    </div>
+
+    <div class="">
+        <h5 class="font-bold mb-4">John Doe</h5>
+
+        <p class="text-sm">
+            Presumanbly, if you are watching this series,
+            you have already made the decision to embrace
+            al the Presumanbly, if you are watching this series,
+            you have already made the decision to embrace
+            al the 
+        </p>
+    </div>
+</div>
